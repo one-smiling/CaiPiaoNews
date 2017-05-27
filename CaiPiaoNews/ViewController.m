712fb33d6b1f3d8189d9b88b138bdae5159ad558
Simay <821748575@qq.com>
2017-05-27 17:52:17 +0800

    创建项目

--- CaiPiaoNews/ViewController.m
+++ CaiPiaoNews/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <WebKit/WebKit.h>
+
 @interface ViewController ()
 
 @end
@@ -16,6 +18,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    WKWebView *webView = [[WKWebView alloc] init];
+    [self.view addSubview:webView];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[webView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(webView)]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[webView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(webView)]];
+    NSURL *url = [NSURL URLWithString:@"http://news.baidu.com/ns?word=彩票&tn=news&from=news&cl=2&rn=20&ct=1"];
+    [webView loadRequest:[NSURLRequest requestWithURL:url]];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
